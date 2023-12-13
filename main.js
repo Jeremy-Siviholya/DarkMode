5e206786dadc057d214deb3dc36db83171d8140d
Jeremy-Siviholya <jeremysiviholya7@outlook.fr>
2023-12-13 08:54:10 +0100

    toogle theme on localstorage

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 
 
 document.querySelector("i").addEventListener("click", () => {
-  document.documentElement.classList.toggle(localStorage.getItem("theme"));
+  document.documentElement.classList.toggle(
+    localStorage.getItem("theme") === "dark"
+      ? localStorage.setItem("theme","light") 
+      : localStorage.setItem("theme","dark")
+  );
 });
+document.documentElement.classList.add(localStorage.getItem('theme'))
